a60635e35298a338e2e10579a9480202c86d71f1
cub8 <caoc.cubas@gmail.com>
2026-04-25 18:43:34 -0600

    nueva funcionalidad

--- hola.py
+++ hola.py
@@ -1,4 +1,16 @@
+!pip install python-dotenv
+
+import os
+from dotenv import load_dotenv #!pip install dotenv
+
+
 print("Hola mundo")
 
 def saludar():
     print("Hola watoncito :3")
+
+clave_super_secreta = os.getenv("llave_api_super_secreta")
+
+def saludar_preguntar():
+    print("Hola watoncito :3, ¿Cómo estás?")
+
