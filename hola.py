227730bfe80a87b811cb62a18c31503cc380a377
cub8 <caoc.cubas@gmail.com>
2026-04-25 18:33:47 -0600

    dotenv

--- hola.py
+++ hola.py
@@ -1,4 +1,10 @@
+import os
+from dotenv import load_dotenv
+
 print("Hola mundo")
 
 def saludar():
     print("Hola watoncito :3")
+
+def saludar_pregunta():
+    print("Hola watoncito :3, ¿Cómo estás?")
